b185f40dfe380e4396e8a1caade092553237b2e6
Keifer Rasner <rasnerkeifer@gmail.com>
2022-07-26 21:01:59 -0500

    Fetch Movies & SignIn requests

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,1 +1,6 @@
-const url = process.env.REACT_SERVER_API;
+import axios from "axios";
+
+const baseURL = process.env.REACT_APP_SERVER_API;
+
+export const SignIn = (credentials) => axios.post(`${baseURL}/auth`, credentials)
+export const FetchMovies = () => axios.get(`${baseURL}/movies`)
