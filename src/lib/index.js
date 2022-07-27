f2834dede217bf43f0a18c3de95f1c32de3361e8
Keifer Rasner <rasnerkeifer@gmail.com>
2022-07-27 15:55:04 -0500

    Registration & Login State/API Calls (Complete)

--- src/lib/index.js
+++ src/lib/index.js
@@ -2,5 +2,20 @@ import axios from "axios";
 
 const baseURL = process.env.REACT_APP_SERVER_API;
 
-export const SignIn = (credentials) => axios.post(`${baseURL}/auth`, credentials)
+export const SignIn = async (credentials) => {
+    try {
+        const response = await axios.post(`${baseURL}/auth`, credentials)
+        return response.data
+    } catch (error) {
+        return { message: error.response.data.message, status: error.response.status }
+    }
+}
+export const CreateNewUser = async (credentials) => {
+    try {
+        const response = await axios.post(`${baseURL}/auth/create`, credentials)
+        return {message: response.data.message, status: response.status};
+    } catch (error) {
+        return { message: error.response.data.message, status: error.response.status }
+    }
+}
 export const FetchMovies = () => axios.get(`${baseURL}/movies`)
